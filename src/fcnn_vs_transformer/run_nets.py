82fc1bcd0551d1e348e41e0605dad5d43573d08e
Roger-Montane <roger.montane.guell@gmail.com>
2023-03-03 11:00:38 -0700

    Small changes tu nets runner

--- src/fcnn_vs_transformer/run_nets.py
+++ src/fcnn_vs_transformer/run_nets.py
@@ -35,14 +35,14 @@ print(f'Passes = {sum(dp.Y_train[:,0])}; Fails = {sum(dp.Y_train[:,1])}')
 fcn_net = FCN(rolling_window_width=dp.rollWinWidth)
 fcn_net.build()
 fcn_net.fit(X_train=dp.X_train, Y_train=dp.Y_train, X_test=dp.X_test, Y_test=dp.Y_test,
-            trainWindows=dp.trainWindows, epochs=20, save_model=True)
+            trainWindows=dp.trainWindows, epochs=200, save_model=True)
 fcn_net.plot_acc_loss()
 fcn_net.compute_confusion_matrix(X_winTest=dp.X_winTest, Y_winTest=dp.Y_winTest, plot=True)
 fcn_net.make_probabilities_plots(X_winTest=dp.X_winTest, Y_winTest=dp.Y_winTest)
 
 transformer_net = Transformer()
 transformer_net.fit(X_train=dp.X_train, Y_train=dp.Y_train, X_test=dp.X_test, Y_test=dp.Y_test,
-                    trainWindows=dp.trainWindows, epochs=3, save_model=True)
+                    trainWindows=dp.trainWindows, epochs=200, save_model=True)
 transformer_net.plot_acc_loss()
 transformer_net.compute_confusion_matrix(X_winTest=dp.X_winTest, Y_winTest=dp.Y_winTest, plot=True)
 transformer_net.make_probabilities_plots(X_winTest=dp.X_winTest, Y_winTest=dp.Y_winTest)
